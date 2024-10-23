b79c295feae1883cce347c8a0bfdf24cd1f56fc4
Baudouin Grolet <117576912+BavouilleDev@users.noreply.github.com>
2024-10-23 16:59:40 +0200

    Create email.js

---
+++ email.js
@@ -0,0 +1,18 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "API_KEY_PLACEHOLDER",
+    });
+})();
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        handleButtonClick()
+        event.preventDefault();
+        emailjs.sendForm('portfolio', 'contact_form', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
